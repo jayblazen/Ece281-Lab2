265e99cf262c10ba71ce0eaf2a2da36994a97d53
jayblazen <70984269+jayblazen@users.noreply.github.com>
2026-03-02 10:20:49 -0700

    maybe maybe test benches were wrong

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -34,7 +34,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg_decoder is
     Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (43 downto 0));
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
